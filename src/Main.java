e6bc2f050215d3ecacfdf88a73049bc4febd7a6d
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-02-26 09:43:14 +1100

    refactored with mentor help

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,53 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] original = {3, 6, 2, 7, 1};
+        int[] original = new int[]{ 5, 3, 2, 8, 1, 4 };
         int[] sorted = sortArray(original);
-        reinsert(original, sorted);
+        int[] finalArray = createOddSortedArray(original.clone(), sorted);
 
+        System.out.println("Final: ");
+        printArray(finalArray);
     }
 
-    private static int[] reinsert(int[] original, int[] sorted) {
+    private static int[] createOddSortedArray(int[] original, int[] sorted) {
         int position = 0;
         int swapPosition = 0;
+
         while (position < original.length) {
-            position = FindNextSwap(sorted, position);
+            position = FindNextOdd(sorted, position);
             swapPosition = FindNextOdd(original, swapPosition);
 
             if (position == -1) break;
 
-
-            sorted[swapPosition] = original[position];
+            original[swapPosition] = sorted[position];
             position++;
             swapPosition++;
         }
 
-
         return original;
-
     }
 
-    private static int FindNextSwap(int[] sorted, int position) {
-        System.out.print("Sorted: ");
-        printArray(sorted);
-        for(int i = position; i < sorted.length; i++) {
-            if (!(sorted[i] % 2 == 0)) {
+
+    private static int FindNextOdd(int[] sorted, int position) {
+        for (int i = position; i < sorted.length; i++) {
+            if (sorted[i] % 2 == 1) {
                 return i;
             }
         }
-
 
         return -1;
     }
 
-    private static int FindNextOdd(int[] original, int position) {
-        System.out.print("Original: ");
-        printArray(original);
-        for(int i = position; i < original.length; i++){
-            if(!(original[i] % 2 == 0)){
-                return i;
-            }
-        }
-
-        // find the next position in the original array to swap into.
-        return 0;
-    }
-
     private static void printArray(int[] sorted) {
-        for(var i : sorted){
+        for (var i : sorted) {
             System.out.print(i + ",");
         }
         System.out.println();
     }
 
-    static int[] sortArray(int[] array) {
+    private static int[] sortArray(int[] array) {
         int[] sorted = array.clone();
         Arrays.sort(sorted);
 
